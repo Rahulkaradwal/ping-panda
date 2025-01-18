c2013199c06ab6b32d8ad1920f23d3b2565f4ed8
rahul karadwal <rahulkaradwal2000@gmail.com>
2025-01-18 15:39:27 -0500

    fix: middleware to protect the routes

--- src/server/procedures.ts
+++ src/server/procedures.ts
@@ -1,20 +1,37 @@
+import { db } from "@/db"
 import { j } from "./__internals/j"
+import { currentUser } from "@clerk/nextjs/server"
+import { HTTPException } from "hono/http-exception"
 
-const authMiddleware = j.middleware(({ next }) => {
-  const user = { name: "Rahul" }
+const authMiddleware = j.middleware(async ({ c, next }) => {
+  const authHeader = c.req.header("Authorization")
+
+  if (authHeader) {
+    const apiKey = authHeader.split(" ")[1] // bearer <API_KEY>
+
+    const user = await db.user.findUnique({
+      where: { apiKey },
+    })
+
+    if (user) {
+      return next({ user })
+    }
+  }
+  const auth = await currentUser()
+  if (!auth) {
+    throw new HTTPException(401, { message: "Unauthorized" })
+  }
+
+  const user = await db.user.findFirst({
+    where: { externalId: auth.id },
+  })
+  if (!user) {
+    throw new HTTPException(401, { message: "Unauthorized" })
+  }
+
   return next({ user })
 })
-/**
- * Middleware for providing a built-in cache with your Prisma database.
- *
- * You can remove this if you don't like it, but caching can massively speed up your database queries.
- */
 
-/**
- * Public (unauthenticated) procedures
- *
- * This is the base piece you use to build new queries and mutations on your API.
- */
 export const baseProcedure = j.procedure
 export const publicProcedure = baseProcedure
 export const privateProcedure = baseProcedure.use(authMiddleware)

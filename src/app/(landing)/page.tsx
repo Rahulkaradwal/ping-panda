6e7226418f266f4674d4a99080b44da0ce24554a
rahul karadwal <rahulkaradwal2000@gmail.com>
2025-01-18 12:41:01 -0500

    feat: review section

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,10 +1,7 @@
-import { Check } from "lucide-react"
-import Heading from "../../components/heading"
-import MaxWidthWrapper from "../../components/max-width-wrapper"
-import { ShinyButton } from "../../components/shiny-button"
 import HeadingSection from "@/components/landing/HeadingSection"
 import DiscordSection from "@/components/landing/DiscordSection"
 import BentoGridSection from "@/components/landing/BentoGridSection"
+import ReviewSection from "@/components/landing/ReviewSection"
 
 const Page = () => {
   return (
@@ -12,8 +9,7 @@ const Page = () => {
       <HeadingSection />
       <DiscordSection />
       <BentoGridSection />
-      <section></section>
-      <section></section>
+      <ReviewSection />
     </>
   )
 }

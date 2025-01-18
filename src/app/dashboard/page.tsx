edfe415c157588e49d26ad68b41fb68724fb71fe
rahul karadwal <rahulkaradwal2000@gmail.com>
2025-01-18 17:17:11 -0500

    feat: dahsboard component

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,28 @@
+import { DashboardPageContent } from "@/components/dashboard/dashboard-page-content"
+import { DashboardPage } from "@/components/dashboard/dashoard-page"
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
 import React from "react"
 
-function page() {
-  return <div>Dashboard</div>
+async function page() {
+  const auth = await currentUser()
+
+  if (!auth) {
+    redirect("/sign-in")
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) {
+    return redirect("/welcome")
+  }
+
+  return (
+    <DashboardPage title="Dashboard">{<DashboardPageContent />}</DashboardPage>
+  )
 }
 
 export default page

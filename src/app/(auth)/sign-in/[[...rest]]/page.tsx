e4e5f28128e0aa6f93ad06361d3a01d711247a25
rahul karadwal <rahulkaradwal2000@gmail.com>
2025-01-18 12:51:07 -0500

    feat: auth pages

---
+++ src/app/(auth)/sign-in/[[...rest]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignIn } from "@clerk/nextjs"
+import React from "react"
+
+function page() {
+  return (
+    <div className="w-full flex-1 flex items-center justify-center">
+      <SignIn />
+    </div>
+  )
+}
+
+export default page
---
+++ src/app/(auth)/sign-up/[[...rest]]/page.tsx
@@ -0,0 +1,12 @@
+import { SignUp } from "@clerk/nextjs"
+import React from "react"
+
+function page() {
+  return (
+    <div className="w-full flex-1 flex items-center justify-center">
+      <SignUp />
+    </div>
+  )
+}
+
+export default page
